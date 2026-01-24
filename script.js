65175d4d3d20c365a68040140fc0ecdc44423b56
alich381 <candrianaalirosid1@gmail.com>
2026-01-25 00:12:12 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function updateTime(){
-  const time = new Date().toLocaleTimeString('id-ID',{
-    timeZone:'Asia/Jakarta'
-  });
-  document.getElementById('time').innerText = time;
-}
-
-setInterval(updateTime,1000);
-updateTime();
